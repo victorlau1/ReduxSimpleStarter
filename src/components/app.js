af837365f0c9c9c693d38e2fc9b0727bdfa98092
victorlau1 <lau.victor.w@gmail.com>
2017-08-24 09:14:25 -0700

    finished lecture 64

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import SearchBar from '../container/search-bar.js';
+import WeatherList from '../container/weatherlist.js';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      <SearchBar/>
+      <WeatherList/>
+      </div>
     );
   }
 }
