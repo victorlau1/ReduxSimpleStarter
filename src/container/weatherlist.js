5c5b04dfef92840ae2ed7a8e265eae8c60b0e0e3
victorlau1 <lau.victor.w@gmail.com>
2017-08-24 12:03:35 -0700

    Finished Demo - Need to fix Google Map

--- src/container/weatherlist.js
+++ src/container/weatherlist.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { connect} from 'react-redux';
+import { connect } from 'react-redux';
+import SparkChart from '../components/sparklines.js';
+import GoogleMap from '../components/google_map.js';
 
 
 class WeatherList extends Component {
     renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
+
         return (
-            <tr>
-                <td>{cityData.city.name}</td>
+            <tr key={name}>
+            <td><GoogleMap lon={lon} lat={lat}/></td>
+            <td><SparkChart data={temps} color="yellow" units="K"/></td>
+            <td><SparkChart data={pressure} color="green" units="hPa"/></td>
+            <td><SparkChart data={humidity} color="blue" units="%"/></td>
             </tr>
         )
     }
@@ -32,7 +43,7 @@ class WeatherList extends Component {
 }
 
 function mapStateToProps({weather}) {
-    return { weather};
+    return { weather: weather };
 }
 
 export default connect(mapStateToProps)(WeatherList);
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -6,5 +6,5 @@ export default function (state = [], action) {
         return [action.payload.data, ...state];
     }
     return state;
-}``
+}
 
